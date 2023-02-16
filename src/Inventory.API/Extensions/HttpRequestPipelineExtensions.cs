536d89a5c049d0e1786d6c8dd1d9e5e80a6105a4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-16 17:35:47 +0530

    Inventory Service

---
+++ src/Inventory.API/Extensions/HttpRequestPipelineExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Inventory.API.Extensions;
+
+public static class HttpRequestPipelineExtensions
+{
+    public static WebApplication ConfigureHttpRequestPipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.MapGet("/", () => "Please use /swagger to see the API documentation.");
+
+        return app;
+    }
+}
