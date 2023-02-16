01517918fee34b11dcac68159286fe34a9d9d096
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-16 17:23:36 +0530

    Controller

---
+++ src/Inventory.API/Controllers/ItemsController.cs
@@ -0,0 +1,61 @@
+﻿using CommonLibrary.Interfaces;
+using Inventory.Data.Dtos;
+using Inventory.Data.Entities;
+using Inventory.Data.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.API.Controllers;
+
+[Route("api/items")]
+[ApiController]
+public class ItemsController : ControllerBase
+{
+    private readonly IRepository<InventoryItem> _itemsRepository;
+
+    public ItemsController(IRepository<InventoryItem> itemsRepository)
+    {
+        _itemsRepository = itemsRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync(Guid userId)
+    {
+        if (userId == Guid.Empty)
+        {
+            return BadRequest();
+        }
+
+        var items = (await _itemsRepository.GetAllAsync(item => item.UserId == userId))
+                    .Select(item => item.AsDto());
+
+        return Ok(items);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> PostAsync(GrantItemsDto grantItemsDto)
+    {
+        var inventoryItem = await _itemsRepository.GetAsync(
+            item => item.UserId == grantItemsDto.UserId && item.CatalogItemId == grantItemsDto.CatalogItemId);
+
+        if (inventoryItem == null)
+        {
+            inventoryItem = new InventoryItem
+            {
+                CatalogItemId = grantItemsDto.CatalogItemId,
+                UserId = grantItemsDto.UserId,
+                Quantity = grantItemsDto.Quantity,
+                AcquiredDate = DateTimeOffset.UtcNow
+            };
+
+            await _itemsRepository.CreateAsync(inventoryItem);
+        }
+        else
+        {
+            inventoryItem.Quantity += grantItemsDto.Quantity;
+            await _itemsRepository.UpdateAsync(inventoryItem);
+        }
+
+        return Ok();
+    }
+
+}
