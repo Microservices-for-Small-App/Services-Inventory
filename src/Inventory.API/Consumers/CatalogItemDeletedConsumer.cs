1cb2fc136539acc9942f88806c0ee1f08b969b82
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-24 22:35:28 +0530

    Consumer Classes

---
+++ src/Inventory.API/Consumers/CatalogItemDeletedConsumer.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Contracts;
+using CommonLibrary.Interfaces;
+using Inventory.Data.Entities;
+using MassTransit;
+
+namespace Inventory.API.Consumers;
+
+public class CatalogItemDeletedConsumer : IConsumer<CatalogItemDeleted>
+{
+    private readonly IRepository<CatalogItem> _catalogItemrepository;
+
+    public CatalogItemDeletedConsumer(IRepository<CatalogItem> catalogItemrepository)
+    {
+        _catalogItemrepository = catalogItemrepository;
+    }
+
+    public async Task Consume(ConsumeContext<CatalogItemDeleted> context)
+    {
+        var message = context.Message;
+
+        var item = await _catalogItemrepository.GetAsync(message.ItemId);
+
+        if (item == null)
+        {
+            return;
+        }
+
+        await _catalogItemrepository.RemoveAsync(message.ItemId);
+    }
+}
