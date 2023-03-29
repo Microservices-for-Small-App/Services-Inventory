1d52d8c663d83e9f7a920ffa1e6c352f6333d185
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-29 22:22:41 +0530

    Update SubtractItemsConsumer.cs

--- src/Inventory.API/Consumers/SubtractItemsConsumer.cs
+++ src/Inventory.API/Consumers/SubtractItemsConsumer.cs
@@ -34,7 +34,17 @@ public class SubtractItemsConsumer : IConsumer<SubtractItems>
 
         if (inventoryItem is not null)
         {
+            if (inventoryItem.MessageIds.Contains(context.MessageId!.Value))
+            {
+                await context.Publish(new InventoryItemsSubtracted(message.CorrelationId));
+
+                return;
+            }
+
             inventoryItem.Quantity -= message.Quantity;
+
+            inventoryItem.MessageIds.Add(context.MessageId.Value);
+
             await _inventoryItemsRepository.UpdateAsync(inventoryItem);
         }
 
